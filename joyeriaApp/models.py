5a4b9074eb8dc2d66c6e0295db03d5c7a8b0f85a
ferchorecalt <ferchorecalt@gmail.com>
2016-02-01 11:56:27 -0300

    Modelo

--- joyeriaApp/models.py
+++ joyeriaApp/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=30)
+
+class Articulo(models.Model):
+    modelo = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=500)
+    fecha = models.DateField()
+    imagen = models.TextField()
+    articulo_marca=models.ForeignKey(Marca, on_delete=models.CASCADE)
