cbaace25d36476ba9ab5651d8214e27fce150b64
ferchorecalt <ferchorecalt@gmail.com>
2016-02-02 11:24:08 -0300

    imagenes andando

--- joyeriaApp/urls.py
+++ joyeriaApp/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^gallery', views.index, name='index'),
-    url(r'^404', views.index, name='index'),
-    url(r'^news', views.index, name='index'),
-    url(r'^mail', views.index, name='mail'),
+    url(r'^gallery', views.gallery, name='gallery'),
+    url(r'^404', views.services, name='services'),
+    url(r'^news', views.news, name='news'),
+    url(r'^mail', views.mail, name='mail'),
 ]
