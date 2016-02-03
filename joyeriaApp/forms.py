6fb584b99e8d2e0a31eb13c78d939712c4971e0c
ilanrosenfeld7 <ilanrosenfeld7@gmail.com>
2016-02-03 10:59:34 -0300

    mejorando forms

--- joyeriaApp/forms.py
+++ joyeriaApp/forms.py
@@ -8,11 +8,15 @@ class ArticuloForm(forms.ModelForm):
             # fields = ('modelo', 'descripcion','imagen','articulo_marca')
             fields = '__all__'
             widgets = {
-                'descripcion': Textarea(attrs={'cols': 40, 'rows': 10}),
+                'descripcion': Textarea(attrs={'cols': 40, 'rows': 10, 'placeholder': 'Ingrese una breve descripcion del articulo a ofrecer'}),
             }
     def __init__(self, *args, **kwargs):
         super(ArticuloForm, self).__init__(*args, **kwargs)
         self.fields['articulo_marca'].label = "Marca"
+        for field in self.fields.values():
+            field.widget.attrs.update({'class' : 'separados'})
+            field.error_messages = {'required':'El campo {fieldname} es requerido'.format(
+                fieldname=field.label)}
 
 class MarcaForm(forms.ModelForm):
     class Meta:
@@ -20,6 +24,10 @@ class MarcaForm(forms.ModelForm):
             fields = ('nombre',)
             error_messages = {
                 'nombre': {
-                    'unique': "El nombre de la marca debe ser unico.",
+                    'unique': "El nombre de la marca ya existe!",
+                    'required':'Debe ingresar un nombre de marca!',
                 },
             }
+    def __init__(self, *args, **kwargs):
+         super(MarcaForm, self).__init__(*args, **kwargs)
+         self.fields['nombre'].widget.attrs.update({'class' : 'separados'})
