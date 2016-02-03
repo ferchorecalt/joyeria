ec7ffd7ae9bb7a006a39d638497b663e5ce8b910
ilanrosenfeld7 <ilanrosenfeld7@gmail.com>
2016-02-03 10:15:07 -0300

    marca metodo __str

--- joyeriaApp/models.py
+++ joyeriaApp/models.py
@@ -6,6 +6,8 @@ from django.db import models
 
 class Marca(models.Model):
     nombre = models.CharField(max_length=30, unique=True)
+    def __str__(self):
+        return '%s' % (self.nombre)
 
 class Articulo(models.Model):
     modelo = models.CharField(max_length=30)
